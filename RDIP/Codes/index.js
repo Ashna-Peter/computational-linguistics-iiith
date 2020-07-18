5eafc88b8cab14047938a9547aac1dbb7b70b6e9
Ashna-Peter <ashnapeter01@gmail.com>
2020-07-18 12:06:48 +0530

    index.js

--- RDIP/Codes/index.js
+++ RDIP/Codes/index.js
@@ -18,7 +18,7 @@ const English = [
     "after eating John went to church",
     "John after eating went to church",
   ],
-   ["did he go to market", "he did go to market"],
+  ["did he go to market", "he did go to market"],
   [
     "the woman who called my sister sells cosmetics",
     "the woman who sells cosmetics called my sister",
@@ -29,8 +29,8 @@ const English = [
     "John goes to the library and studies",
     "John studies and goes to the library",
   ],
-   ["John ate an apple so did she", "she ate an apple so did John"],
-   [
+  ["John ate an apple so did she", "she ate an apple so did John"],
+  [
     "the teacher returned the book after she noticed the error",
     "the teacher noticed the error after she returned the book",
     "after the teacher returned the book she noticed the error",
@@ -40,7 +40,7 @@ const English = [
     "after she returned the book the teacher noticed the error",
     "after she noticed the error the teacher returned the book",
   ],
- [
+  [
     "I told her that I bought a book yesterday",
     "I told her yesterday that I bought a book",
     "yesterday I told her that I bought a book",
@@ -49,70 +49,72 @@ const English = [
     "yesterday I bought a book that I told her",
   ],
 ];
-const Hindi = [[
-  "राम और श्याम बाजार गयें",
-  "राम और श्याम गयें बाजार",
-  "बाजार गयें राम और श्याम",
-  "गयें बाजार राम और श्याम",
-],
-[
-  "राम सोया और श्याम भी",
-  "श्याम सोया और राम भी",
-  "सोया श्याम और राम भी",
-  "सोया राम और श्याम भी",
-],
-[
-  "मैंने उसे बताया कि राम सो रहा है",
-  "मैंने उसे बताया कि सो रहा है राम",
-  "उसे मैंने बताया कि राम सो रहा है",
-  "उसे मैंने बताया कि सो रहा है राम",
-  "मैंने बताया उसे कि राम सो रहा है",
-  "मैंने बताया उसे कि सो रहा है राम",
-  "उसे बताया मैंने कि राम सो रहा है",
-  "उसे बताया मैंने कि सो रहा है राम",
-  "बताया मैंने उसे कि राम सो रहा है",
-  "बताया मैंने उसे कि सो रहा है राम",
-  "बताया उसे मैंने कि राम सो रहा है",
-  "बताया उसे मैंने कि सो रहा है राम",
-],
-[
-  "राम खाकर सोया",
-  "खाकर राम सोया",
-  "राम सोया खाकर",
-  "खाकर सोया राम",
-  "सोया राम खाकर",
-  "सोया खाकर राम",
-],
-[
-  "बिल्लियों को मारकर कुत्ता सो गया",
-  "मारकर बिल्लियों को कुत्ता सो गया",
-  "बिल्लियों को मारकर सो गया कुत्ता",
-  "मारकर बिल्लियों को सो गया कुत्ता",
-  "कुत्ता सो गया बिल्लियों को मारकर",
-  "कुत्ता सो गया मारकर बिल्लियों को",
-  "सो गया कुत्ता बिल्लियों को मारकर",
-  "सो गया कुत्ता मारकर बिल्लियों को",
-],
-[
-  "एक लाल किताब वहाँ है",
-  "एक लाल किताब है वहाँ",
-  "वहाँ है एक लाल किताब",
-  "है वहाँ एक लाल किताब",
-],
-[
-  "एक बड़ी सी किताब वहाँ है",
-  "एक बड़ी सी किताब है वहाँ",
-  "बड़ी सी एक किताब वहाँ है",
-  "बड़ी सी एक किताब है वहाँ",
-  "वहाँ है एक बड़ी सी किताब",
-  "वहाँ है बड़ी सी एक किताब",
-  "है वहाँ एक बड़ी सी किताब",
-  "है वहाँ बड़ी सी एक किताब",
-] ];
-let Erandom,Hrandom;
+const Hindi = [
+  [
+    "राम और श्याम बाजार गयें",
+    "राम और श्याम गयें बाजार",
+    "बाजार गयें राम और श्याम",
+    "गयें बाजार राम और श्याम",
+  ],
+  [
+    "राम सोया और श्याम भी",
+    "श्याम सोया और राम भी",
+    "सोया श्याम और राम भी",
+    "सोया राम और श्याम भी",
+  ],
+  [
+    "मैंने उसे बताया कि राम सो रहा है",
+    "मैंने उसे बताया कि सो रहा है राम",
+    "उसे मैंने बताया कि राम सो रहा है",
+    "उसे मैंने बताया कि सो रहा है राम",
+    "मैंने बताया उसे कि राम सो रहा है",
+    "मैंने बताया उसे कि सो रहा है राम",
+    "उसे बताया मैंने कि राम सो रहा है",
+    "उसे बताया मैंने कि सो रहा है राम",
+    "बताया मैंने उसे कि राम सो रहा है",
+    "बताया मैंने उसे कि सो रहा है राम",
+    "बताया उसे मैंने कि राम सो रहा है",
+    "बताया उसे मैंने कि सो रहा है राम",
+  ],
+  [
+    "राम खाकर सोया",
+    "खाकर राम सोया",
+    "राम सोया खाकर",
+    "खाकर सोया राम",
+    "सोया राम खाकर",
+    "सोया खाकर राम",
+  ],
+  [
+    "बिल्लियों को मारकर कुत्ता सो गया",
+    "मारकर बिल्लियों को कुत्ता सो गया",
+    "बिल्लियों को मारकर सो गया कुत्ता",
+    "मारकर बिल्लियों को सो गया कुत्ता",
+    "कुत्ता सो गया बिल्लियों को मारकर",
+    "कुत्ता सो गया मारकर बिल्लियों को",
+    "सो गया कुत्ता बिल्लियों को मारकर",
+    "सो गया कुत्ता मारकर बिल्लियों को",
+  ],
+  [
+    "एक लाल किताब वहाँ है",
+    "एक लाल किताब है वहाँ",
+    "वहाँ है एक लाल किताब",
+    "है वहाँ एक लाल किताब",
+  ],
+  [
+    "एक बड़ी सी किताब वहाँ है",
+    "एक बड़ी सी किताब है वहाँ",
+    "बड़ी सी एक किताब वहाँ है",
+    "बड़ी सी एक किताब है वहाँ",
+    "वहाँ है एक बड़ी सी किताब",
+    "वहाँ है बड़ी सी एक किताब",
+    "है वहाँ एक बड़ी सी किताब",
+    "है वहाँ बड़ी सी एक किताब",
+  ],
+];
+let Erandom, Hrandom;
 function fun() {
- selection = document.getElementById("selectlang");
- let store = "";
+  selection = document.getElementById("selectlang");
+  let store = "";
   if (selection.value === "English") {
     document.getElementById("form_sentence").innerHTML =
       "<b>Form a sentence (Declarative or Interrogative or any other type) from the given words</b>" +
@@ -130,7 +132,7 @@ function fun() {
     startingvalue = 0;
     updatedvalue = 0;
     for (i = 0; i <= word.length - 1; i++) {
-       temp =
+      temp =
         "<button  id='btn" +
         i +
         "' onclick='reply_click(this.id,this.value)' value='" +
@@ -142,8 +144,7 @@ function fun() {
       updatedvalue++;
     }
     document.getElementById("btnval").innerHTML = store;
-  }
-  else if (selection.value === "Hindi") {
+  } else if (selection.value === "Hindi") {
     document.getElementById("form_sentence").innerHTML =
       "<b>Form a sentence (Declarative or Interrogative or any other type) from the given words</b>" +
       "</br>" +
@@ -160,7 +161,7 @@ function fun() {
     startingvalue = 0;
     updatedvalue = 0;
     for (i = 0; i <= word.length - 1; i++) {
-       temp =
+      temp =
         "<button  id='btn" +
         i +
         "' onclick='reply_click(this.id,this.value)' value='" +
@@ -172,14 +173,13 @@ function fun() {
       updatedvalue++;
     }
     document.getElementById("btnval").innerHTML = store;
-  }
-  else {
+  } else {
     document.getElementById("form_sentence").innerHTML = "";
     document.getElementById("val").innerHTML = "";
     document.getElementById("formed_sentence").innerHTML = "";
     document.getElementById("print").innerHTML = "";
     document.getElementById("reform").innerHTML = "";
-    document.getElementById("check").innerHTML=""
+    document.getElementById("check").innerHTML = "";
     $("#reset").hide();
   }
 }
@@ -194,17 +194,17 @@ function shuffle(sentence) {
   }
   return words;
 }
-function reply_click(clicked_id,clicked_value) {
+function reply_click(clicked_id, clicked_value) {
   document.getElementById("formed_sentence").innerHTML =
-    "<b>Formed Sentence</b>"+" <i>(after selecting words)</i>";
+    "<b>Formed Sentence</b>" + " <i>(after selecting words)</i>";
   document.getElementById("print").innerHTML += clicked_value + " ";
   $("#reset").show();
   document.getElementById(clicked_id).style.display = "none";
   startingvalue++;
-  if(updatedvalue == startingvalue){
-    $("#correct").show()
-  //sentence=document.getElementById("print").innerHTML;
-  //alert(sentence);
+  if (updatedvalue == startingvalue) {
+    $("#correct").show();
+    //sentence=document.getElementById("print").innerHTML;
+    //alert(sentence);
   }
 }
 function reform() {
@@ -214,63 +214,71 @@ function reform() {
   document.getElementById("formed_sentence").innerHTML = "";
   document.getElementById("print").innerHTML = "  ";
   document.getElementById("reform").innerHTML = "";
-  document.getElementById("check").innerHTML = ""
+  document.getElementById("check").innerHTML = "";
   startingvalue = 0;
   $("#reset").hide();
-  $("#correct").hide()
+  $("#correct").hide();
 }
 function correctness() {
-  let check_sentence=document.getElementById("print").innerHTML
-//  console.log(check_sentence);
-  str_sentence=check_sentence.toString();
+  let check_sentence = document.getElementById("print").innerHTML;
+  //  console.log(check_sentence);
+  str_sentence = check_sentence.toString();
+  sentence = str_sentence.trim();
   //alert(sentence);
-  console.log(str_sentence);
- //var flag=false;
-   if (selection.value=="English") {
+  console.log(sentence);
+  //var flag=false;
+  if (selection.value == "English") {
     // console.log(lang);
-     for(value in English[Erandom]){
-      value=English[Erandom]
-              //console.log(value);
+
+    value = English[Erandom];
+    console.log(value);
     //  console.log(value);
-      var count=value.length;
-      console.log(count);
-      for(var i=0;i<count;i++){
-        let  temp=value[i]
-        console.log(temp);
-        if(temp==str_sentence){
-          //flag = true;
-          document.getElementById("check").innerHTML="Right answer!!!";
-          break;
-      //console.log("correct");
-        }
-        else {
-          document.getElementById("check").innerHTML="Wrong answer!!!";
-      //console.log("rng");
+    var count = value.length;
+  //  console.log(count);
+    for (var i = 0; i < count; i++) {
+      let check = value[i];
+      console.log(temp);
+      let correct = check.localeCompare(sentence);
+      if (correct === 0) {
+        //flag = true;
+        document.getElementById("check").innerHTML = "Right answer!!!";
+        document.getElementById("check").style.color = "green";
+        return (document.getElementById("check").innerHTML = "Right answer!!!");
+        //break;
+        //console.log("correct");
+      }
+      {
+        document.getElementById("check").innerHTML = "Wrong answer!!!";
+        document.getElementById("check").style.color = "red";
+        //console.log("rng");
+      }
+    }
   }
-}
-}}
-else if (selection.value=="Hindi") {
- // console.log(lang);
-  for(value in Hindi[Hrandom]){
-   value=Hindi[Hrandom]
-           //console.log(value);
-           console.log(value);
-   var count=value.length;
-   console.log(count);
-   for(var i=0;i<count;i++){
-     temp=count[i]
-         //console.log(value[i]);
-     if(temp==check_sentence){
-       //flag = true;
-      consile.log(temp);
-       document.getElementById("check").innerHTML="correct sentence";
-       break;
-   //console.log("correct");
-     }
-     else {
-       document.getElementById("check").innerHTML="Wrong";
-   //console.log("rng");
-}
-}
-}}
+  else if (selection.value == "Hindi") {
+    // console.log(lang);
+
+    value = Hindi[Hrandom];
+    console.log(value);
+    //  console.log(value);
+    var count = value.length;
+    console.log(count);
+    for (var i = 0; i < count; i++) {
+      let check = value[i];
+      console.log(temp);
+      let correct = check.localeCompare(sentence);
+      if (correct === 0) {
+        //flag = true;
+        document.getElementById("check").innerHTML = "Right answer!!!";
+        document.getElementById("check").style.color = "green";
+        return (document.getElementById("check").innerHTML = "Right answer!!!");
+        //break;
+        //console.log("correct");
+      }
+      {
+        document.getElementById("check").innerHTML = "Wrong answer!!!";
+        document.getElementById("check").style.color = "red";
+        //console.log("rng");
+      }
+    }
+  }
 }
